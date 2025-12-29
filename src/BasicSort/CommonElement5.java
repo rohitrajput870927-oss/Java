68d269a484306ba10ee7094b4a8f348dcb3bef4e
rohitrajput870927-oss <rohitrajput870927@gmail.com>
2025-12-29 23:37:48 +0530

    CommonElement

--- src/BasicSort/CommonElement5.java
+++ src/BasicSort/CommonElement5.java
@@ -6,6 +6,8 @@ public class CommonElement5 {
         int[] a = {1, 1, 1, 2, 3, 5, 5};
         int[] b = {1, 1, 2, 4, 4, 6, 8};
              //Ohdbbxzbb
+
+        //it is use to create c  arr
         int[] c = new int[Math.min(a.length, b.length)];//returns the smaller length of the two arrays.
 
         int i = 0, j = 0, k = 0;
