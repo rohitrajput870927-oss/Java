2c7a13994401855514c6e0395a378fe6fab155ac
rohitrajput870927-oss <rohitrajput870927@gmail.com>
2026-03-21 16:34:56 +0530

    Pick Finding

--- src/Arrays/prectice36.java
+++ src/Arrays/prectice36.java
@@ -1,5 +1,15 @@
 package Arrays;
 
 public class prectice36 {
+    static void main() {
+        int[] arr={5,2,1,4};
+        int sum=0;
+        int a=arr.length+1;
+        for(int i=0;i< arr.length;i++){
+            sum=sum+arr[i];
+        }
+        int sumi=(a*(a+1))/2;
+        System.out.println(sum-sumi);
+    }
 
 }
